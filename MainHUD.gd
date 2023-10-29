de74dd48559992c44b583423ececaf72f9a2c13d
WHEOfficial <46579241+WHEOfficial@users.noreply.github.com>
2023-10-29 13:00:30 -0400

    Made night timer work

--- MainHUD.gd
+++ MainHUD.gd
@@ -9,11 +9,17 @@ var power = 1000
 var elapsedTime = 0
 
 var elapsedMinute = 0
-var elapsedHour = 12
+var elapsedHour: int = 12
 
 
 func _on_timer_timeout():
 	elapsedTime += 1
 	
 	elapsedMinute = elapsedTime % 60
-	#elapsedHour = (int) elapsedTime / 60
+	elapsedHour = (elapsedTime / 60 + 12) % 12
+	if elapsedHour == 0:
+		elapsedHour = 12
+		
+	elapsedMinute = "%02d" % elapsedMinute
+	
+	hourDisplay.text = str(elapsedHour) + ":" + elapsedMinute + " AM"
